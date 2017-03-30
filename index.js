07441dd6130674216de90bcb634577980df1a5d9
Adam Dąbrowski <gipsio@gmail.com>
2017-03-31 00:55:22 +0200

    Improved sorting of leads JS
    Now if lead led to TYP it is not counted in lead count for the purpose
    of reporting

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class csvAnalyzer {
             return /DART/.test(e[3]);
         });
         this.publishers = this.parsedFile.data.filter(function (e) {
-            return !/DART/.test(e[3]) && !/PARTNERSTWA/.test(e[3]) && /Getin/.test(e[0]) && e[5];
+            return !/DART/.test(e[3]) && !/PARTNERSTWA/.test(e[3]) && /(TYP|Lead|LP)/.test(e[0]) && e[5];
         });
         let output = document.getElementById("output");
         const countPublishers = this.countOcurrence(this.publishers);
@@ -29,12 +29,18 @@ class csvAnalyzer {
     }
 
     countOcurrence(fromData, container = []) { // go for render here
-        for (let i = 0; i < fromData.length; i++) {
-            if (container.indexOf(fromData[i][0]) == -1) {
-                container.push(fromData[i][0]);
+        let long = fromData.filter(function (e) {
+            return /Długi/.test(e[0]) && /- TYP/.test(e[0]);
+        });
+       console.log(this.longShortCheck(fromData, long));
+       let checkedData = this.longShortCheck(fromData);
+        //define regex for lead dlugi
+        for (let i = 0; i < checkedData.length; i++) {
+         if (container.indexOf(checkedData[i][0]) == -1) {
+                container.push(checkedData[i][0]);
                 container.push(1);
             } else {
-                container[container.indexOf(fromData[i][0]) + 1]++;
+                container[container.indexOf(checkedData[i][0]) + 1]++;
             }
         }
         let render = container.map(function (current, index) {
@@ -51,14 +57,23 @@ class csvAnalyzer {
         return render;
     }
 
-    longShortCheck (short, long, productType){
-        // logic for krotki dlugi wniosek
-        
+    longShortCheck (all, long){//or maybe all of it and delete true shorts from it
+        let checkedAll = all;
+        console.log(long);
+        for (let i = all.length-1; i >= 0; i--) {
+            if(long != undefined || long != null){
+            if (long.find(function(e){if(!long){return undefined;} else return e[5] == all[i][5];}) && /Lead/.test(checkedAll[i])){
+                checkedAll.splice(i, 1);//to chyba znaczy ze moze omijac moze lepiej od tylu puscic
+            }
+        }
+        }
+       // console.log(checkedAll);
+         return checkedAll;        
     }
 
     initiateParse() {
         this.reader = new FileReader();
-        console.log(this);
+      //  console.log(this);
         this.reader.readAsText(this.preParsed);
         this.reader.onload = this.loadHandler.bind(this);
         this.reader.onerror = this.errorHandler;
