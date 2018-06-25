63f526a835bc7dc8c87a2ce56a66b95fb1005019
Adam Dąbrowski <gipsio@gmail.com>
2018-06-25 17:33:00 +0200

    clean and export to file data from DCM

--- index.js
+++ index.js
@@ -7,109 +7,36 @@ class csvAnalyzer {
         //date collection
     }
 
-    interpretData(oneDayCSV) { //full csv is loaded
+    interpretData(data) { //full csv is loaded
+       let csvExport = 'data:text/csv;charset=utf-8,';
+        const startIndex = data.data.findIndex((el) => { return el[0] === 'Report Fields' })+1;
+        //FIND INDEX OF RESULTS: DONE
+        console.log(startIndex);
+        const endIndex = data.data.findIndex((el) => { return el[0] === 'Grand Total:' });
+        console.log(data.data, endIndex);
+        const exportDcm = data.data.slice(startIndex, endIndex);
+        exportDcm.forEach((el) => {
+            el.unshift('DCM');
+            let row = el.join(',');
+            csvExport += row + '\r\n';
 
-        let dates = this.filterDays(this.parsedFile.data);
-        console.log(dates);
-        //change this parsedFile to data from one day
-        for (let i = 0; i < dates.length; i++) {
-            // console.log(date, typeof date);
-            this.partnerships = this.parsedFile.data.filter(function (e) {
-                return /PARTNERSTWA/.test(e[3]) && dates[i].includes(e[4]);
-            });
-            this.dartGoogle = this.parsedFile.data.filter(function (e) {
-                return /DART/.test(e[3]) && dates[i].includes(e[4]);
-            });
-            this.publishers = this.parsedFile.data.filter(function (e) {
-                return !/DART/.test(e[3]) && !/PARTNERSTWA/.test(e[3]) && /(TYP|Lead|LP)/.test(e[0]) && e[5] && dates[i].includes(e[4]);
-            });
-            let output = document.getElementById("output");
-            const countPublishers = this.countOcurrence(this.publishers);
-            //  console.log(countPublishers);
-            const countSearch = this.countOcurrence(this.dartGoogle);
-            const countPartnerships = this.countOcurrence(this.partnerships);
-            output.innerHTML += `<h1>Day: ${dates[i]}</h1>
-            <tr><th>Wydawcy</th></tr> ${countPublishers}
-            <tr><th>Partnerstwa</th></tr> ${countPartnerships}
-            <tr><th>Google</th></tr> ${countSearch}`;
+        });//works
+      //  console.log(exportDcm, csvExport);
+ 
 
-        }
+        //FIND INDEX OF GRAND TOTAL DONE
+        //APPEND COLUMN DCM        
+        //return console.log(data);
+        const encodedURI = encodeURI(csvExport);
+       // window.open(encodedURI);//POP-up Warning
 
-        //console.log(countSearch);
-        //console.log(countPartnerships);
-        //now it's time to render
-    }
 
-    filterDays(data) {
-        let dateCollection = [];
 
-        for (let i = 0; i < data.length; i++) {
-            if (dateCollection.indexOf(data[i][4]) == -1 && /\d\d\d\d-\d\d-\d\d/.test(data[i][4])) { //regex works fine
-                dateCollection.push(data[i][4]);
-            }
-        }
-
-        console.log(dateCollection, "before sort");
-
-        dateCollection.sort((a, b) => {
-
-           // a = Number(a.split('-').join(''));
-          //  b = Number(b.split('-').join(''));
-            console.log(a);
-            return new Date(a) - new Date(b);
-        }); // sort dates 
-        console.log(dateCollection, "after sort");
-        return dateCollection; //works good
-    }
-
-    countOcurrence(fromData, container = []) { // go for render here
-        let long = fromData.filter(function (e) {
-            return /Długi/.test(e[0]) && /- TYP/.test(e[0]);
-        });
-        //console.log(this.longShortCheck(fromData, long));
-        let checkedData = this.longShortCheck(fromData);
-        //define regex for lead dlugi
-        for (let i = 0; i < checkedData.length; i++) {
-            if (container.indexOf(checkedData[i][0]) == -1) { //if product doesn't exist in arr, push its name and first ocurrence to it
-                container.push(checkedData[i][0]);
-                container.push(1);
-            } else {
-                container[container.indexOf(checkedData[i][0]) + 1]++;
-            }
-        }
-        //counting ended 
-        let render = container.map(function (current, index) {
-            if (index % 2 === 0) {
-                return "<tr><td>" + current + "</td>";
-            } else {
-                return "<td>" + current + "</td></tr>";
-
-            }
-        });
-
-        // output.innerHTML += container.toString().replace(/,/g, "<br>");
-        //console.log(render);
-        return render;
-    }
-
-    longShortCheck(all, long) { //or maybe all of it and delete true shorts from it
-        //this function deletes short leads if a short lead led to long lead
-        let checkedAll = all;
-        //console.log(long);
-        for (let i = all.length - 1; i >= 0; i--) {
-            if (long != undefined || long != null) {
-                if (long.find(function (e) {
-                        if (!long) {
-                            return undefined;
-                        } else return e[5] == all[i][5];
-                    }) &&
-                    /Lead/.test(checkedAll[i])) {
-                    checkedAll.splice(i, 1); //because how splice works, loop is working from the end towards the beginning
-                }
-            }
-        }
-        // console.log(checkedAll);
-        return checkedAll;
+        const link = document.createElement('a');
+        link.setAttribute('href', encodedURI);
+        link.setAttribute('download', 'csv_export.csv');
+        link.innerHTML = 'Download cleaned data';
+        document.body.appendChild(link);//append link with filename and csv extension
     }
 
     initiateParse() {
@@ -124,7 +51,7 @@ class csvAnalyzer {
     }
     loadHandler(event) {
         this.parsedFile = Papa.parse(event.target.result); //Papa provided from other file
-        this.interpretData();
+        this.interpretData(this.parsedFile);
     }
 }
 
