757f2e1b53e84376df2bee7b94410f8956efbfbd
Adam Dąbrowski <gipsio@gmail.com>
2018-07-18 13:06:07 +0200

    Append columns specific for DCM

--- index.js
+++ index.js
@@ -12,28 +12,42 @@ class csvAnalyzer {
         //index with header fields
         return el[0] === "Report Fields";
       }) + 1;
+    const endIndex = data.data.findIndex(el => {
+      //index of last row with data Grand total sum
+      return el[0] === "Grand Total:";
+    });
+    const placemendIdIndex = data.data[startIndex].findIndex(el => {
+      return el === "Placement ID";
+    });
     const placementIndex = data.data[startIndex].findIndex(el => {
       //index of column with placement name
       return el === "Placement";
     });
+    const adIdIndex = data.data[startIndex].findIndex(el => {
+      return el === "Ad ID";
+    })
     const adIndex = data.data[startIndex].findIndex(el => {
       //index of column containing Ad name
       return el === "Ad";
     });
-    const endIndex = data.data.findIndex(el => {
-      //index of last row with data Grand total sum
-      return el[0] === "Grand Total:";
+    const siteIdIndex = data.data[startIndex].findIndex(el => {
+      return el === "Site ID";
+    })
+    const clickUrlIndex = data.data[startIndex].findIndex(el => {
+      return el === "Click-through URL";
     });
     const exportDcm = data.data.slice(startIndex, endIndex);
     exportDcm.forEach((el, i) => {
       el[adIndex] = el[adIndex].replace(/[,]/g, "");
       el[placementIndex] = el[placementIndex].replace(/[,]/g, "");
+      el[clickUrlIndex] = el[clickUrlIndex].replace(/["]/g, "");
+      console.log(el[clickUrlIndex]);
       el[el.length - 1] = el[el.length - 1].replace(/.00/, "");
       el.unshift("DCM");
-      let row = el.join(", ");
+      let row = el.join(",");// test without space
       if (i == exportDcm.length - 1) {
         csvExport += row;
-        csvExport += "";//SREDNIK
+        csvExport += "\r\n"; //SREDNIK
       } else {
         csvExport += row;
         csvExport += "\r\n";
